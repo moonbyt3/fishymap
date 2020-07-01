61532d55ab67f376cceaf9711e50f4053917fd14
moonbyt3 <moonbyt3@gmail.com>
2020-07-01 20:21:19 +0200

    popup hardcoded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import MapView from './components/MapView';
+import MapView from './components/MapView/MapView';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <MapView/>
+      <MapView />
     </div>
   );
 }
---
+++ src/components/MarkerPopup/MarkerPopup.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {Popup} from 'react-leaflet';
+
+import {
+  MarkerPopupWrapper,
+  MarkerPopupTitle,
+  MarkerPopupMediaWrapper,
+  MarkerPopupMedia,
+  MarkerPopupData,
+  MarkerPopupDataItem,
+  MarkerPopupDataItemTitle,
+  MarkerPopupDataItemText,
+} from './MarkerPopup.css';
+
+import FishImage from '../../assets/img/fish.png';
+import Avatar from '../../assets/img/icons/avatar.png'
+
+const MarkerPopup = (props) => {
+  const { name } = props.data;
+  console.log(name);
+
+  return  (
+    <Popup>
+      <MarkerPopupWrapper>
+        <MarkerPopupMediaWrapper>
+          <MarkerPopupTitle>{name}</MarkerPopupTitle>
+          <MarkerPopupMedia>
+            <img src={FishImage} alt=""/>
+          </MarkerPopupMedia>
+        </MarkerPopupMediaWrapper>
+        <MarkerPopupData>
+          <MarkerPopupDataItem>
+            <MarkerPopupDataItemTitle>Težina</MarkerPopupDataItemTitle>
+            <MarkerPopupDataItemText>1.6kg</MarkerPopupDataItemText>
+          </MarkerPopupDataItem>
+          <MarkerPopupDataItem>
+            <MarkerPopupDataItemTitle>Vrsta</MarkerPopupDataItemTitle>
+            <MarkerPopupDataItemText>Štuka</MarkerPopupDataItemText>
+          </MarkerPopupDataItem>
+          <MarkerPopupDataItem>
+            <MarkerPopupDataItemTitle>Vrijeme</MarkerPopupDataItemTitle>
+            <MarkerPopupDataItemText>08:00</MarkerPopupDataItemText>
+          </MarkerPopupDataItem>
+          <MarkerPopupDataItem>
+            <img src={Avatar} alt=""/>
+          </MarkerPopupDataItem>
+        </MarkerPopupData>
+        
+      </MarkerPopupWrapper>
+    </Popup>
+  );
+};
+
+export default MarkerPopup;
