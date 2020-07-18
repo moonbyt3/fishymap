2b3a99374b7b2b68ab9c2f203f951c581ad1358a
moonbyt3 <moonbyt3@gmail.com>
2020-07-18 15:26:44 +0200

    Updates

---
+++ precache-manifest.98acd368178e54e370675d075a250c5a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f70b0810fd2b21d62b03d221f3cee5",
+    "url": "/fishymap/index.html"
+  },
+  {
+    "revision": "89f649006ac3cd4583d8",
+    "url": "/fishymap/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "55dc9e9c810f8d08f9c5",
+    "url": "/fishymap/static/css/main.d902555f.chunk.css"
+  },
+  {
+    "revision": "89f649006ac3cd4583d8",
+    "url": "/fishymap/static/js/2.05bbad88.chunk.js"
+  },
+  {
+    "revision": "1a81bf1ab93b99b2e1311a5fb73f500a",
+    "url": "/fishymap/static/js/2.05bbad88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55dc9e9c810f8d08f9c5",
+    "url": "/fishymap/static/js/main.8a37751c.chunk.js"
+  },
+  {
+    "revision": "42ff20da1030f8f4b127",
+    "url": "/fishymap/static/js/runtime-main.e55ccf65.js"
+  },
+  {
+    "revision": "695933203a90bcc2c83b1fed53eab17d",
+    "url": "/fishymap/static/media/fish.69593320.jpg"
+  },
+  {
+    "revision": "6bc3fb704ba60a7d130df9ace02cae4c",
+    "url": "/fishymap/static/media/fish.6bc3fb70.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fishymap/precache-manifest.2ec5cee8d202b610588db33b1df0d3ff.js"
+  "/fishymap/precache-manifest.98acd368178e54e370675d075a250c5a.js"
 );
 
 self.addEventListener('message', (event) => {
