ed8a8d9ec008b229a2e8d7ce7dfbae5198473a98
moonbyt3 <moonbyt3@gmail.com>
2020-07-19 16:08:01 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fishymap/precache-manifest.18843731685148d830923ecc1a647b6b.js"
+  "/fishymap/precache-manifest.2a18de73500f9352ce41ddd5ba3d3a56.js"
 );
 
 self.addEventListener('message', (event) => {
